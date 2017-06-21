2b2593a3b91b77684be8416c1123cafb484f1b6a
John Eccleston <john.eccleston@capgemini.com>
2017-06-21 10:21:54 +0100

    Refactored code, to remove vars

--- src/main/scala/Bill.scala
+++ src/main/scala/Bill.scala
@@ -5,56 +5,36 @@ import scala.math.BigDecimal.RoundingMode
   */
 case class Bill(items: Array[String]) {
 
-  private var bill = BigDecimal(0.00)
-  private var serviceCharge = BigDecimal(0.00)
-  private var servicePercent = BigDecimal(0.00)
-  private var isFood = false
-  private var isHot = false
-
-  private val menu = Map(
+  val menu = Map(
     ("Cola" -> (BigDecimal(0.50), false, false)),
     ("Coffee" -> (BigDecimal(1.00), false, true)),
     ("Cheese Sandwich" -> (BigDecimal(2.00), true, false)),
     ("Steak Sandwich" -> (BigDecimal(4.50), true, true))
   )
 
-  private def getTotal(price: BigDecimal, food: Boolean, hot: Boolean) = (price, food, hot) match {
+  // Sum menu items input
+  def getBill() = { items.flatMap(menu.get).foldLeft(BigDecimal(0.00))((soFar, current) => soFar + current._1) }
 
-    case (p, false, _) => bill += p
-    case (p, true, false) => bill += p; isFood = true
-    case(p, true, true) => bill += p; isFood = true; isHot = true
-  }
+  // return true if menu items input contain food
+  def isFood(): Boolean = { items.flatMap(menu.get).find(x=> x._2 == true).nonEmpty }
 
-  private def getService(): BigDecimal = {
+  // return true if menu items input contain hot food
+  def isHotFood(): Boolean = { items.flatMap(menu.get).find(x=> x._2 == true && x._3 == true).nonEmpty }
+
+  // return service charge up to a maximum of 20%, service charge is based on whole bill
+  def getService(): BigDecimal = {
     // if it is a drink, no service charge
     // if it is cold food add 10% to service charge
     // if it is hot food add 20% service charge
-    if(isFood) servicePercent = if(isHot) BigDecimal(0.2) else BigDecimal(0.1)
+    val servicePercent = if(isFood) if(isHotFood) BigDecimal(0.2) else BigDecimal(0.1) else BigDecimal(0.0)
 
-    serviceCharge = bill*servicePercent
     // Maximum service charge is 20.00
-    if (serviceCharge <= BigDecimal(20.00))
-      serviceCharge.setScale(2, RoundingMode.HALF_UP)
+    if (retBill*servicePercent <= BigDecimal(20.00))
+      retBill*servicePercent.setScale(2, RoundingMode.HALF_UP)
     else BigDecimal(20.00)
   }
 
-  if(items.nonEmpty || items == null) {
-    for (item <- items) {
-      try {
-        val full = menu(item)
-        getTotal(full._1, full._2, full._3)
-      } catch {
-        case nse: NoSuchElementException => println(s"Invalid Menu Item: $item")
-      }
-    }
-  } else {
-    println("No Items in Menu")
-    val retBill = bill.setScale(2)
-    val retService = getService()
-    val retTotal = bill + retService
-  }
-
-  val retBill = bill.setScale(2)
+  val retBill = getBill().setScale(2)
   val retService = getService()
-  val retTotal = bill + retService
+  val retTotal = retBill + retService
 }
